2f583e0b6a871d8c17fdedc82cd0aa28e4963b49
ethicnology <25751709+ethicnology@users.noreply.github.com>
2020-05-09 15:57:16 +0200

    Update episode6.py

--- episode6.py
+++ episode6.py
@@ -1,50 +1,20 @@
-#episode 6
-import math
 import re
-import tkinter as tk
+try:
+    f = open('./episode_6/ep6.txt', "r")
+    f.seek(0)
+    first_char = f.read(1)
+    if not first_char:
+        print("file is empty")
+    else:
+        f.seek(0)
+        for x in f:
+            temp = re.findall('\d+', x)
+            res = list(map(int, temp))
+            if res[0] < res[1]:
+                print("[oui] la première valeur : ", res[0], "est inferieur à la deuxième valeur :", res[1])
+            else:
+                print("[non] la première valeur : ", res[0], "est superieur à la deuxième valeur :", res[1])
 
-
-class Application(tk.Frame):
-  def __init__(self, master=None):
-    super().__init__(master)
-    self.master = master
-    self.pack()
-    self.create_widgets()
-
-  def create_widgets(self):
-    self.source = tk.Label(self)
-    self.source["text"] = "Source :"
-    self.source.pack(side="top")
-
-    self.input = tk.Entry(self)
-    self.input.pack(side="top")
-
-    self.hi_there = tk.Button(self)
-    self.hi_there["text"] = "Clean"
-    self.hi_there["command"] = self.say_hi
-    self.hi_there.pack(side="top")
-
-    self.dest = tk.Label(self)
-    self.dest["text"] = "Destination :"
-    self.dest.pack(side="top")
-
-    self.inputleft = tk.Entry(self)
-
-    self.inputleft.pack(side="left")
-
-    self.inputright = tk.Entry(self)
-    self.inputright.pack(side="right")
-
-  def say_hi(self):
-    str = self.input.get()
-    temp = re.findall(r'\d+', str)
-    res = list(map(int, temp))
-    self.inputleft.insert(0, res[0])
-    self.inputleft.insert("end" , "cm")
-    self.inputright.insert(0, res[1])
-    self.inputright.insert("end", "cm")
-
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+    f.close()
+except FileNotFoundError:
+    print('File does not exist')
