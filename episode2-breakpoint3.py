b0f78f7e0002d2ce984af48dfbec1ac2d7414dca
ethicnology <ethicnology@pm.me>
2020-05-09 12:28:47 +0200

    episode2 breakpoint3 exo 17

---
+++ episode2-breakpoint3.py
@@ -0,0 +1,26 @@
+# 15
+liste = [17, 38, 10, 25, 72]
+liste.sort()
+liste.append(12)
+liste.reverse()
+print(liste)
+print(liste.index(17))
+liste.remove(38)
+print(liste)
+print(liste[1:3])
+print(liste[:2])
+print(liste[2:])
+print(set(liste))
+
+# 16
+chaine = "abcdefghijklmnopqrstuvwxyz" [::-1]
+print(chaine)
+
+# 17
+chaine = str(input("Enter word : "))
+if chaine == chaine[::-1]:
+    print("This is a palindrome")
+else:
+    print(chaine[::-1])
+
+# 18
